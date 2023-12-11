fd755b5ba5a315e5fa80f0b2fd9ed96762080ed9
Jack Brummer <jbrummer@stevens.edu>
2023-12-11 14:52:20 -0500

    rusty chatbot

--- Rust/chatbot/rich-chat/src/app.rs
+++ Rust/chatbot/rich-chat/src/app.rs
@@ -2,34 +2,59 @@ use leptos::*;
 use leptos_meta::*;
 use leptos_router::*;
 
+mod components;
+
+
 use crate::model::conversation::Conversation;
 
 #[component]
-pub fn App() -> impl IntoView {
+pub fn App(cx: Scope) -> impl IntoView {
     // Provides context that manages stylesheets, titles, meta tags, etc.
     provide_meta_context();
 
     let (conversation, set_conversation) = create_signal(cx, Conversation::new());
 
-    let send = crate.action(cx, move |new_message: &String| {
+    let send = create_action(move |new_message: &String| {
         let user_message = Message {
             text: new_message.clone(),
             user: true,
         };
         set_conversation.update(move |c| {
-            c.message.push(user_message);
-        })
+            c.messages.push(user_message);
+        });
+        converse(conversation.get())
     });
+
+    create_effect(move |_| {
+        if let Some(_) = send.input().get() {
+            let model_message = Message {
+                text: String::from("..."),
+                user:false,
+
+            };
+
+            set_conversation.update(move |c| {
+                c.messages.push(model_message);
+            });
+        } 
+    });
+
+    create_effect(move |_| {
+        if let Some(Ok(response)) = send.value().get() {
+            set_conversation.update(move |c| {
+                c.messages.last_mut().unwrap().text = response;
+            })
+        }
+    });
+
     view! {
         // injects a stylesheet into the document <head>
         // id=leptos means cargo-leptos will hot-reload this stylesheet
         <Stylesheet id="leptos" href="/pkg/leptos_start.css"/>
-
         // sets the document title
-        <Title text="Welcome to Leptos"/>
+        <Title text="Welcome to rich chat"/>
         <ChatArea conversation />
         <TypeArea send />
-        
     }
 }
 
---
+++ Rust/chatbot/rusty_bot/src/main.rs
@@ -0,0 +1,21 @@
+use winit::{
+    event_loop::EventLoop,
+    window::{Window, WindowBuilder},
+};
+
+fn main() {
+    let event_loop = EventLoop::new();
+    let window = WindowBuilder::new().build(&event_loop).unwrap();
+
+    event_loop.run(move |event, _, control_flow| {
+        *control_flow = winit::event_loop::ControlFlow::Wait;
+
+        match event {
+            winit::event::Event::WindowEvent {
+                event: winit::event::WindowEvent::CloseRequested,
+                ..
+            } => *control_flow = winit::event_loop::ControlFlow::Exit,
+            _ => (),
+        }
+    });
+}
