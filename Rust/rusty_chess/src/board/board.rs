b255b218d941c3b2e0fe3f4b925346c2e61ddd28
Jack Brummer <jbrummer402@gmail.com>
2023-12-12 22:55:30 -0500

    alternating board space colors

--- Rust/rusty_chess/src/board/board.rs
+++ Rust/rusty_chess/src/board/board.rs
@@ -3,6 +3,8 @@ use raylib::prelude::*;
 pub struct Space {
     x_pos: u8,
     y_pos: u8,
+    width: u8,
+    height: u8,
     taken: bool,
 }
 
@@ -20,7 +22,7 @@ impl Board {
     pub fn draw(&self, mut d: RaylibDrawHandle, thread: &RaylibThread) {
         for row in 0..8 {
             for col in 0..8 { 
-                d.draw_rectangle(20 * row, 20 * col, 10, 10, Color::BLACK);
+                d.draw_rectangle(20 * row, 20 * col, 15, 15, if (col + row) % 2 == 0 { Color::BLACK} else {Color::GRAY });
             }
         }
     }
