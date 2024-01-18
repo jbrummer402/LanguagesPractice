004c6ad716145775c926e99d937bbf33dd4ccf2b
Jack Brummer <jbrummer@stevens.edu>
2024-01-18 18:54:35 -0500

    Update piece.rs
    need to fix loading rectangles

--- Rust/rusty_chess/src/board/piece.rs
+++ Rust/rusty_chess/src/board/piece.rs
@@ -26,8 +26,9 @@ impl Behvaior for PieceName {
 pub struct Piece {
     pub owner: bool,
     pub name: PieceName,
+    pub piece_rect: Rectangle,
     pub piece_texture: Texture2D,
-    pub position: (u8, u8),
+    game: &Game,
 }
 
 
@@ -61,13 +62,54 @@ impl Piece {
         }
     }
 
-    pub fn new(owner: bool, name: PieceName, piece_texture: Texture2D, position: (u8, u8), ) -> Piece {
+    pub fn load_rects(rl: &mut RaylibHandle, layout: [[u8; 8]; 8]) -> Vec<Rectangle>{
+        let mut piece_rects = Vec::with_capacity(32);
+
+       for row in 0..layout.len() {
+            for col in 0..layout[row].len() {
+                
+                let space = layout[row][col];
+
+                let owner = space / 64;
+                let piece = (space / 8) % 8;
+                let location  = space % 8;                    
+                
+                if owner == 0 {
+                    continue;
+                }
+
+                piece_rects.push(Rectangle {
+                                    x: location as f32 * 60.0,
+                                    y: row as f32 * 60.0,
+                                    width: 60.0,
+                                    height: 60.0,
+                                });
+            }
+
+        }
+          
+        return piece_rects;
+    }
+
+    pub fn new(owner: bool, name: PieceName, piece_rect: Rectangle,  piece_texture: Texture2D, game: &Game) -> Piece {
+
+        // Make the rectangle for the piece on initialization
+        println!("{:?}", owner);
+        raylib::
+        let piece_rect = Rectangle {
+                    x:
+                    y,
+                    width,
+                    height,
+                 }
+
         Piece {
             owner: owner,
             name: name,
             piece_texture: piece_texture,
-            position: position, 
-           
+            piece_rect: piece_rect
+            
+            game: game, 
         }
     }
-}
+
