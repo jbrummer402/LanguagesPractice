e6a7d4f05e2792e388fddd10f9f93a3d2767976a
Jack Brummer <jbrummer@stevens.edu>
2024-01-20 13:20:04 -0500

    mapping piece index to name

--- Rust/rusty_chess/src/board/piece.rs
+++ Rust/rusty_chess/src/board/piece.rs
@@ -1,5 +1,5 @@
-use raylib::prelude::*;
 use raylib::consts::MouseButton::*;
+use raylib::prelude::*;
 
 #[derive(Debug, Eq, Hash, Copy, Clone, PartialEq)]
 pub enum PieceName {
@@ -11,105 +11,97 @@ pub enum PieceName {
     King,
 }
 
-
 trait Behvaior {
     fn can_move(&self);
 }
 
 impl Behvaior for PieceName {
-    fn can_move(&self) {
-    
-    }
+    fn can_move(&self) {}
 }
-
 
 pub struct Piece {
     pub owner: bool,
     pub name: PieceName,
     pub piece_rect: Rectangle,
     pub piece_texture: Texture2D,
-    game: &Game,
 }
-
 
 trait Movement {
     fn can_move(self);
 }
 
 impl Movement for PieceName {
-    fn can_move(self) {
-        
-    }
+    fn can_move(self) {}
 }
 
 impl Piece {
-    pub fn draw_self(&self, d: &mut RaylibDrawHandle) {
-        d.draw_texture(&self.piece_texture, self.position.0.into(), self.position.1.into(), color::Color::WHITE);
-    }
-
-    pub fn _move(self, piece_rect: &mut Rectangle, d: &mut RaylibDrawHandle, rl: &mut RaylibHandle) {
+    pub fn _move(
+        self,
+        piece_rect: &mut Rectangle,
+        d: &mut RaylibDrawHandle,
+        rl: &mut RaylibHandle,
+    ) {
         let mut dragging = false;
         let mut offset = Vector2::default();
         if rl.is_mouse_button_pressed(MOUSE_LEFT_BUTTON) {
             if piece_rect.check_collision_point_rec(rl.get_mouse_position()) {
                 dragging = true;
-                offset.x = rl.get_mouse_x() as f32 - piece_rect.x as f32; 
+                offset.x = rl.get_mouse_x() as f32 - piece_rect.x as f32;
                 offset.y = rl.get_mouse_y() as f32 - piece_rect.y as f32;
             }
-        } 
-        else if rl.is_mouse_button_released(MOUSE_LEFT_BUTTON) {
+        } else if rl.is_mouse_button_released(MOUSE_LEFT_BUTTON) {
             dragging = false;
         }
     }
-
-    pub fn load_rects(rl: &mut RaylibHandle, layout: [[u8; 8]; 8]) -> Vec<Rectangle>{
+    pub fn piece_to_name(index: u8) -> PieceName {
+        match index {
+            1 => PieceName::Pawn,
+            2 => PieceName::Rook,
+            3 => PieceName::Knight,
+            4 => PieceName::Bishop,
+            5 => PieceName::Queen,
+            6 => PieceName::King,
+            _ => PieceName::Pawn,
+        }
+    }
+    pub fn load_rects(rl: &mut RaylibHandle, layout: [[u8; 8]; 8]) -> Vec<Rectangle> {
         let mut piece_rects = Vec::with_capacity(32);
 
-       for row in 0..layout.len() {
+        for row in 0..layout.len() {
             for col in 0..layout[row].len() {
-                
                 let space = layout[row][col];
 
                 let owner = space / 64;
                 let piece = (space / 8) % 8;
-                let location  = space % 8;                    
-                
+                let location = space % 8;
+
                 if owner == 0 {
                     continue;
                 }
 
                 piece_rects.push(Rectangle {
-                                    x: location as f32 * 60.0,
-                                    y: row as f32 * 60.0,
-                                    width: 60.0,
-                                    height: 60.0,
-                                });
+                    x: location as f32 * 60.0,
+                    y: row as f32 * 60.0,
+                    width: 60.0,
+                    height: 60.0,
+                });
             }
-
         }
-          
+
         return piece_rects;
     }
 
-    pub fn new(owner: bool, name: PieceName, piece_rect: Rectangle,  piece_texture: Texture2D, game: &Game) -> Piece {
-
-        // Make the rectangle for the piece on initialization
-        println!("{:?}", owner);
-        raylib::
-        let piece_rect = Rectangle {
-                    x:
-                    y,
-                    width,
-                    height,
-                 }
-
+    pub fn new(
+        owner: bool,
+        name: PieceName,
+        piece_rect: Rectangle,
+        piece_texture: Texture2D,
+    ) -> Piece {
         Piece {
             owner: owner,
             name: name,
             piece_texture: piece_texture,
-            piece_rect: piece_rect
-            
-            game: game, 
+            piece_rect: piece_rect,
         }
     }
-
+}
--- Rust/rusty_chess/src/game.rs
+++ Rust/rusty_chess/src/game.rs
@@ -1,12 +1,12 @@
-use raylib::prelude::*;
 use crate::board::piece::{Piece, PieceName};
-use std::collections::HashMap;
-use raylib::consts::MouseButton::*;
-use std::{panic, u8};
-use std::fmt::Error;
 use glob::glob;
+use raylib::consts::MouseButton::*;
+use raylib::prelude::*;
+use std::collections::HashMap;
+use std::fmt::{write, Error};
+use std::{panic, u8};
 
-fn string_to_piece_name(s: &str) -> Result::<PieceName, Error> {
+fn string_to_piece_name(s: &str) -> Result<PieceName, Error> {
     match s {
         "pawn" | "Pawn" => Ok(PieceName::Pawn),
         "rook" | "Rook" => Ok(PieceName::Rook),
@@ -30,118 +30,97 @@ impl Game {
         for row in 0..8 {
             for col in 0..8 {
                 d.draw_rectangle(
-                    60 * row, 
+                    60 * row,
                     60 * col,
-                    60, 
-                    60, 
-                    if (col + row) % 2 == 0 { color::Color::LIGHTGRAY} else { color::Color::GRAY });
+                    60,
+                    60,
+                    if (col + row) % 2 == 0 {
+                        color::Color::LIGHTGRAY
+                    } else {
+                        color::Color::GRAY
+                    },
+                );
             }
         }
-
     }
-    // 
+    //
     pub fn draw_pieces(&self, d: &mut RaylibDrawHandle, piece_rects: &Vec<Rectangle>) {
-        
         // Iterate through every piece currently on the board
         for row in 0..self.layout.len() {
             for col in 0..self.layout[row].len() {
-                
                 let space = self.layout[row][col];
 
                 let owner = space / 64;
                 let piece = (space / 8) % 8;
-                let location  = space % 8;                    
-              
-                
-                
-                // Map the owner to the respective color 
+                let location = space % 8;
+
+                // Map the owner to the respective color
                 // The piece to the respective texture
-                // The location to the respective space              
-                let p_text: &_ = match piece {
-                    1 => {
-                        &self.piece_textures.get(&PieceName::Pawn).unwrap()[owner as usize - 1]
-                    },
-                    2 => {
-                        &self.piece_textures.get(&PieceName::Rook).unwrap()[owner as usize - 1]
-                    },
-                    3 => {
-                        &self.piece_textures.get(&PieceName::Knight).unwrap()[owner as usize - 1]
-                    },
-                    4 => {
-                        &self.piece_textures.get(&PieceName::Bishop).unwrap()[owner as usize - 1]
-                    },
-                    5 => {
-                        &self.piece_textures.get(&PieceName::Queen).unwrap()[owner as usize - 1]
-                    },
-                    6 => {
-                        &self.piece_textures.get(&PieceName::King).unwrap()[owner as usize - 1]
-                    },
-                    _ => continue,
-                };
-                       
+                // The location to the respective space
+                let p_text: &_ = &self
+                    .piece_textures
+                    .get(&(Piece::piece_to_name(piece)))
+                    .unwrap()[owner as usize - 1];
+
                 for rv in piece_rects {
-                    
-                        d.draw_texture_pro(
-                            p_text, 
-                            Rectangle {
-                                x:0.0,
-                                y:0.0,
-                                width: p_text.width() as f32,
-                                height: p_text.height() as f32, },
-                            rv,
-                            Vector2 {x: 0.0, y: 0.0},
-                            0.0,
-                            Color::WHITE,
-                        ); 
-                    
+                    d.draw_texture_pro(
+                        p_text,
+                        Rectangle {
+                            x: 0.0,
+                            y: 0.0,
+                            width: p_text.width() as f32,
+                            height: p_text.height() as f32,
+                        },
+                        rv,
+                        Vector2 { x: 0.0, y: 0.0 },
+                        0.0,
+                        Color::WHITE,
+                    );
+                }
+            }
+        }
+    }
+
+    fn piece_index(str: &String) -> (Result<usize, Error>, u8) {
+        if str.contains("white") {
+            (Ok(str.find("white").unwrap()), 1)
+        } else {
+            (Ok(str.find("black").unwrap()), 2)
+        }
+    }
+
+    pub fn load_pieces_textures(
+        &mut self,
+        rl: &mut RaylibHandle,
+        thread: RaylibThread,
+    ) -> Result<(), Error> {
+        for file in glob("./imgs/pieces-basic-png/*").expect("Directory not found") {
+            let f = file.unwrap();
+            let texture = rl.load_texture(
+                &thread,
+                f.clone().into_os_string().to_str().expect("failed"),
+            );
+            let t = texture.unwrap();
+
+            let path_substring = &(f.clone().into_os_string().into_string().unwrap());
+
+            let (index, owner) = Self::piece_index(&String::from(path_substring));
+
+            let name = string_to_piece_name(&path_substring[index? + 6..path_substring.len() - 4]);
+
+            let p_name = &name.unwrap();
+
+            match self.piece_textures.entry(*p_name) {
+                std::collections::hash_map::Entry::Occupied(mut entry) => {
+                    entry.get_mut().push(t);
                 }
 
-               
+                std::collections::hash_map::Entry::Vacant(mut entry) => {
+                    let mut texture_vec = Vec::<Texture2D>::new();
+                    texture_vec.push(t);
+                    self.piece_textures.insert(*p_name, texture_vec);
+                }
             }
-        } 
-    }
-    
-    fn piece_index(str: &String) -> (Result<usize, Error>, u8) {
-        if str.contains("white") { 
-            (Ok(str.find("white").unwrap()), 1)
-        } 
-        else { (Ok(str.find("black").unwrap()), 2) }
-    } 
-
-    pub fn load_pieces_textures(&mut self, rl: &mut RaylibHandle, thread: RaylibThread) -> Result::<(), Error> {
-        for file in 
-            glob("./imgs/pieces-basic-png/*")
-            .expect("Directory not found") {
-                    let f = file.unwrap();    
-                    let texture = rl.load_texture(&thread, 
-                                                    f 
-                                                    .clone()
-                                                    .into_os_string()
-                                                    .to_str()
-                                                    .expect("failed"));
-                    let t = texture.unwrap();
-
-                    let path_substring = &(f.clone().into_os_string().into_string().unwrap());
-
-                    let (index, owner) = Self::piece_index(&String::from(path_substring));             
-                    
-                    let name = string_to_piece_name(&path_substring[index? + 6..path_substring.len() - 4]);
-
-                    let p_name = &name.unwrap();
-
-                    match self.piece_textures.entry(*p_name) {
-                        std::collections::hash_map::Entry::Occupied(mut entry) => {
-                            entry.get_mut().push(t);
-                        },
-
-                        std::collections::hash_map::Entry::Vacant(mut entry) => {
-                            let mut texture_vec = Vec::<Texture2D>::new();
-                            texture_vec.push(t);
-                            self.piece_textures.insert(*p_name, texture_vec);
-                        },
-
-                    }
-
         }
         Ok(())
     }
@@ -149,30 +128,27 @@ impl Game {
     pub fn run(&mut self, rl: &mut RaylibHandle, thread: RaylibThread) -> Result<(), Error> {
         // Load all the textures for each piece first
         self.load_pieces_textures(rl, thread.clone())?;
-        
+
         let mut piece_rects = Piece::load_rects(rl, self.layout);
-        
+
         let mut dragging = false;
         let mut offset = Vector2::default();
-        
+
         while !(rl.window_should_close()) {
             // Begin drawing the textures after loading
             let mut d: &mut RaylibDrawHandle<'_> = &mut rl.begin_drawing(&thread);
             // clear background each frame
             d.clear_background(Color::WHITE);
-            
+
             // Draw the board and it's alternating color spaces
             // pass over mutable reference to draw handle
-            
+
             self.draw_board(&mut d);
             self.draw_pieces(d, &piece_rects);
-
-        };
+        }
         Ok(())
-        
     }
     pub fn default() -> Game {
-        
         let mut layout: [[u8; 8]; 8] = [[0; 8]; 8];
         let mut piece_order = vec![2, 3, 4, 5, 6, 4, 3, 2];
 
@@ -186,38 +162,36 @@ impl Game {
             for col in 0..layout[row.unwrap() as usize].len() {
                 // Position of the piece
                 layout[row.unwrap() as usize][col] += col as u8;
-                
+
                 let owner: u8 = match row {
                     // If the rows are the top two, the owner is player 1
-                    Some(0) | Some(1) => { 1 },
+                    Some(0) | Some(1) => 1,
                     // elif the rows are the bottom two, the owner is player 2
-                    Some(6) | Some(7) => { 2 },
-                    _ => { 0 }
+                    Some(6) | Some(7) => 2,
+                    _ => 0,
                 };
-                
+
                 layout[row.unwrap() as usize][col] += owner * 64;
 
                 match row {
                     Some(0) | Some(7) => {
                         layout[row.unwrap() as usize][col] += piece_order[col] * 8;
-                    },        
+                    }
                     Some(1) | Some(6) => {
                         layout[row.unwrap() as usize][col] += 8;
-                    },
+                    }
                     _ => {
                         continue;
-                    },
+                    }
                 };
-                
             }
         }
         Game {
             turn: true,
             layout: layout,
             piece_textures: HashMap::<PieceName, Vec<Texture2D>>::new(),
-    // Map the names of pieces to the number of them left
-    // per player
+            // Map the names of pieces to the number of them left
+            // per player
         }
     }
 }
-    
